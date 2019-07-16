eca8c218a30df51e1cb604011327917ace5ae185
huichang9697 <akvs75@naver.com>
2019-07-17 00:27:07 +0900

    part11 에러남... 고쳐야됨

--- app/src/main/java/com/example/sns_project00/activity/WritePostActivity.java
+++ app/src/main/java/com/example/sns_project00/activity/WritePostActivity.java
@@ -2,6 +2,7 @@ package com.example.sns_project00.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
@@ -23,15 +24,30 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class WritePostActivity extends BasicActivity{
     private static final String TAG="WritePostActivity";
     private FirebaseUser user;
-
+    private ArrayList<String> pathList= new ArrayList<>(); //이미지경로들을 담을 곳(이미지경로들을 알아야.. Storage에 그 경로들을 가지고 올릴 수가 있다.)
+    private LinearLayout parent;
+    private int pathCount, successCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_post);
+
+        parent=findViewById(R.id.contentsLayout);
 
         findViewById(R.id.check).setOnClickListener(onClickListener);
         findViewById(R.id.image).setOnClickListener(onClickListener);
@@ -45,8 +61,8 @@ public class WritePostActivity extends BasicActivity{
             case 0 :{                                                                  //myStartActivity메소드에서 requestCode를 0으로 보낸다고 해서
                 if(resultCode == Activity.RESULT_OK){
                     String profilePath = data.getStringExtra("profilePath");
+                    pathList.add(profilePath);
 
-                    LinearLayout parent=findViewById(R.id.contentsLayout);
                     ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
 
                     ImageView imageView = new ImageView(WritePostActivity.this);
@@ -69,7 +85,7 @@ public class WritePostActivity extends BasicActivity{
         public void onClick(View v) {
             switch (v.getId()){
                 case R.id.check:
-                    profileUpdate();
+                    storageUpload();
                     break;
                 case R.id.image:
                     myStartActivity(GalleryActivity.class,"image");
@@ -82,16 +98,61 @@ public class WritePostActivity extends BasicActivity{
     };
 
 
-    private void profileUpdate() {
+    private void storageUpload() {
         final String title = ((EditText) findViewById(R.id.titleEditText)).getText().toString();
         final String contents = ((EditText) findViewById(R.id.contentsEditText)).getText().toString();
 
 
         if (title.length() > 0 && contents.length()>0) {
+            final ArrayList<String> contentsList=new ArrayList<>();
             user = FirebaseAuth.getInstance().getCurrentUser();
-            WriteInfo writeInfo = new WriteInfo(title,contents,user.getUid());    //user.getUid()  = 로그인한 사용자
-            uploader(writeInfo);
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
 
+            for(int i=0; i<parent.getChildCount(); i++){   //안에 자식 뷰만큼 반복을 할거다
+                View view=parent.getChildAt(i); //자식뷰먼저 저장해주고!!(parent(부모) 안에 있는 뷰에 하나하나 접근을 할거다)
+                if(view instanceof EditText){ //view가 만약에 EditText이면
+                    String text= ((EditText)view).getText().toString();
+                    if(text.length()>0){
+                        contentsList.add(text);
+                    }
+                }else {
+                    contentsList.add(pathList.get(pathCount));
+                    final StorageReference mountainImagesRef = storageRef.child("users/"+user.getUid()+"/"+pathCount+".jpg");   //★★사용자의 Uid 가져와서 각각 넣음(구별)★★
+                    try{
+                        InputStream stream = new FileInputStream(new File(pathList.get(pathCount)));
+                        StorageMetadata metadata = new StorageMetadata.Builder().setCustomMetadata("index",""+pathCount).build();    //API에 사용 법 있음.StorageMetadata
+                        UploadTask uploadTask = mountainImagesRef.putStream(stream);
+                        uploadTask.addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                // Handle unsuccessful uploads
+                            }
+                        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                final int index=Integer.parseInt(taskSnapshot.getMetadata().getCustomMetadata("index"));
+                                mountainImagesRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        Log.e("로그","uri : "+uri);  //사진 -> uri
+                                        contentsList.add(index,uri.toString());
+                                        successCount++;
+                                        if(pathList.size()==successCount){
+                                            //완료
+                                            WriteInfo writeInfo = new WriteInfo(title,contentsList,user.getUid(),new Date());    //user.getUid()  = 로그인한 사용자
+                                            storeUpload(writeInfo);
+                                        }
+                                    }
+                                });
+                            }
+                        });
+                    }catch (FileNotFoundException e){
+                        Log.e("로그","에러 : "+e.toString());
+                    }
+                    pathCount++;
+                }
+            }
         }else {
             Toast.makeText(getApplicationContext(), "회원정보를 입력해주세요.", Toast.LENGTH_LONG).show();
         }
@@ -99,7 +160,7 @@ public class WritePostActivity extends BasicActivity{
 
 
 
-    private void uploader(WriteInfo writeInfo){
+    private void storeUpload(WriteInfo writeInfo){
         FirebaseFirestore db = FirebaseFirestore.getInstance(); //firestore 초기화
         db.collection("posts").add(writeInfo)     //firestore에 추가(add)
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
